3caaf5383cc8e4c3324533c986f9c672836aa6fb
iagocarvalho <71970033+iagocarvalho07@users.noreply.github.com>
2023-05-18 08:54:05 -0300

    Update desafio84.py

--- mundo 3/desafio84.py
+++ mundo 3/desafio84.py
@@ -7,13 +7,30 @@ C) Uma listagem com as pessoas mais leves.
 
 """
 
-pessoas = list()
-nomeIdade = list()
+pessoas = []
 while True:
-    nome = input("digite o nome: ")
+    nome = input('Digite o nome: ')
     peso = float(input('Digite o peso: '))
     pessoas.append([nome, peso])
     resp = input('Quer continuar? [S/N] ').strip().upper()[0]
-    if resp in "Nn":
+    if resp == 'N':
         break
-print(f"foram cadastradas {len(pessoas)} pessoas")
+    
+print(f'Foram cadastradas {len(pessoas)} pessoas.')
+maior = menor = pessoas[0][1]
+maior_pessoas = []
+menor_pessoas = []
+for p in pessoas:
+    if p[1] > maior:
+        maior = p[1]
+    if p[1] < menor:
+        menor = p[1]
+for p in pessoas:
+    if p[1] == maior:
+        maior_pessoas.append(p[0])
+    if p[1] == menor:
+        menor_pessoas.append(p[0])
+print(f'O maior peso foi de {maior}kg. Peso de {maior_pessoas}.')
+print(f'O menor peso foi de {menor}kg. Peso de {menor_pessoas}.')
+
+
